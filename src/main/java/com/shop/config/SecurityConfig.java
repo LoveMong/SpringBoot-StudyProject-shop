e782a638ca0288f5812fa57176cb3c3222cc2807
LeeMong <izpzgi2019@gmail.com>
2022-08-14 19:18:59 +0900

    페이지 권한 설정하기

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -39,6 +40,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) // 로그아웃 URL 설정
                 .logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 URL을 설정
         ;
+
+        http.authorizeRequests() // 시큐리티 처리에 HttpServletRequest를 이용
+                .mvcMatchers("/", "/members/**", "item/**", "/images/**").permitAll() // 메인, 회원 관련, 상품 관련, 상품 이미지 관련 페이지는 모든 사용자가 로그인(인증)없이 접근 가능
+                .mvcMatchers("/admin/**").hasRole("ADMIN") // admin으로 시작하는 경로는 계정이 ADMIN일 경우에만 접근 가능
+                .anyRequest().authenticated() // 위 mvcMatchers로 설정해준 결로 외 나머지 경로들은 모두 인증 요구
+        ;
+
+        http.exceptionHandling() // 인증되지 않은 사용자가 리소스에 접근했을 때 수행되는 핸들러 등록
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+        ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception { // static 디렉터리의 하위 파일은 인증 무시하도록 설정
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
     // 비밀번호 데이터베이스에 그대로 저장했을 경우, 데이터베이스가 해킹당하면 고객의 회원정보가 그대로 노출
