dcee95a649ba03e7f033372336501590efd3a896
LeeMong <izpzgi2019@gmail.com>
2022-08-22 15:26:46 +0900

    지연 로딩 설정 및 테스트

--- src/test/java/com/shop/entity/OrderTest.java
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -3,6 +3,7 @@ package com.shop.entity;
 import com.shop.constant.ItemSellStatus;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderItemRepository;
 import com.shop.repository.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,6 +36,9 @@ class OrderTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
 
     public Item createItem() {
         Item item = new Item();
@@ -104,5 +108,23 @@ class OrderTest {
         order.getOrderItems().remove(0);
         em.flush();
 
+    }
+
+    @Test
+    @DisplayName("지연 로딩 테스트")
+    public void lazyLoadingTest() {
+        Order order = this.createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+
+        // 영속성 컨텍스트의 상태 초기화 후 order 엔티티에 저장했던 주문 상품 아이디를 이용하여 orderItem을 데이터베이스에서 다시 조회
+        OrderItem orderItem = orderItemRepository.findById(orderItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        // orderItem 엔티티에 있는 order 객체의 클래스를 출력한다.
+        System.out.println("Order class : " + orderItem.getOrder().getClass());
+        System.out.println("=========================");
+        orderItem.getOrder().getOrderDate();
+        System.out.println("=========================");
     }
 }
